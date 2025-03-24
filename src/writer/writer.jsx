239d313df59830ac3f6b51bfdb6cec754fe6bc5e
PorterNilsson <porter.nilsson@gmail.com>
2025-03-24 11:47:37 -0600

    Made the writer page for individual writers actually reflect a bio and image which is dynamically fetched by the frontend from the service

--- src/writer/writer.jsx
+++ src/writer/writer.jsx
@@ -8,12 +8,14 @@ export function Writer() {
   const [writerBio, setWriterBio] = React.useState('');
 
     React.useEffect(() => {
-      // Should be a service call to database to get the writer's information and image
-      setImageUrl('../writer.jpg');
-      setWriterBio('Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, cupiditate voluptatibus fugit ea beatae at tempore quibusdam quas aut! Accusantium eveniet, magnam eum praesentium officia fugiat dicta quibusdam reiciendis nobis, quisquam ullam nisi laborum. Dolorem voluptatibus reprehenderit odio debitis. Incidunt, voluptatem sit numquam ut, odit explicabo, fuga sapiente consectetur nemo laborum dolore at accusantium nesciunt ipsum libero. Amet expedita impedit porro blanditiis repellendus officia illo. Esse corporis doloribus laboriosam, voluptatum autem temporibus dignissimos ipsum sed quibusdam. Unde quasi cum totam tempora excepturi nam id. Debitis deserunt aliquid dolorem molestias recusandae.');
-    
-      // ADD CODE HERE TO USE THE WRITER NAME TO GET THE WRITER's bio and image
-      
+
+      fetch('/api/writersFollowed')
+      .then((response) => response.json())
+      .then((writers) => {
+        const writer = writers.find(w => w.writer === writerName);
+        setWriterBio(writer.bio);
+        setImageUrl(`../${writer.image}.jpg`);
+      });
       
     }, []);
 
